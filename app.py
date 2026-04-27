e056f2cfe74c2751196eac4ecc3100f23bc99fc9
laser528 <Lazerking528@gmail.com>
2026-04-26 20:08:27 -0400

    Header

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ from dash import Dash, html
 app = Dash(__name__)
 server = app.server
 
-app.layout = html.Div("Hello Dash")
+app.layout = html.Div(children=[
+        html.H1("Machine Learning Dashboard"),
+
+        html.P(
+             "This dashboard will guide users through uploading data, "
+             "choosing a target variable, training models, and making predictions on stock data."
+        ),
+
+        html.Hr(),
+
+    ])
 
 if __name__ == "__main__": 
     app.run(debug=True)
